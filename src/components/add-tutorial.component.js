1d8bf0964c546b2a2a107238b184c71153f5e1c8
konohaQB <akimotoyujin@gmail.com>
2023-12-19 22:57:48 +0900

    fix: update component

--- src/components/add-tutorial.component.js
+++ src/components/add-tutorial.component.js
@@ -4,18 +4,15 @@ import TutorialDataService from "../services/tutorial.service";
 export default class AddTutorial extends Component {
   constructor(props) {
     super(props);
-    
     this.onChangeTitle = this.onChangeTitle.bind(this);
     this.onChangeDescription = this.onChangeDescription.bind(this);
     this.saveTutorial = this.saveTutorial.bind(this);
     this.newTutorial = this.newTutorial.bind(this);
-
     this.state = {
       id: null,
       title: "",
       description: "", 
       published: false,
-
       submitted: false
     };
   }
@@ -61,7 +58,6 @@ export default class AddTutorial extends Component {
       title: "",
       description: "",
       published: false,
-
       submitted: false
     });
   }
